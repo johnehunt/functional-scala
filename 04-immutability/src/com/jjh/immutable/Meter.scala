357ef7918790f5206f5be17d198ccfc5c903483c
John Hunt <john.hunt10@gmail.com>
2020-06-19 14:05:31 +0100

    Immutable examples

---
+++ 04-immutability/src/com/jjh/immutable/Meter.scala
@@ -0,0 +1,18 @@
+package com.jjh.immutable
+
+case class Meter(value: Double) extends AnyVal {
+
+  def +(m: Meter) : Meter = Meter(value + m.value)
+  def -(m: Meter) : Meter = Meter(value - m.value)
+
+}
+
+object MeterSampleApp extends App {
+  println("Creating Value instances")
+  val x = Meter(3.4)
+  val y = Meter(4.3)
+  println("Adding Value instances together")
+  val z = x + y
+  println("Result: " + z)
+
+}
---
+++ 04-immutability/src/com/jjh/immutable/Product.scala
@@ -0,0 +1,36 @@
+package com.jjh.immutable
+
+trait Immutable
+
+trait Product extends Immutable {
+  val price: Double
+}
+
+case class Version(val number: Int) extends Immutable {
+  override def toString = "v" + number
+
+  def next: Version = copy(number + 1)
+}
+
+case class Invoice[T <: Product](id: Int,
+                                 name: String,
+                                 contents: List[T],
+                                 version: Version = Version(0)) extends Immutable {
+  def total: Double = contents.map(_.price).sum
+
+  def +(p: T): Invoice[T] = Invoice(id, name, p :: contents, version.next)
+
+  def -(p: T): Invoice[T] = Invoice(id, name, contents diff List(p), version.next)
+}
+
+case class Book(title: String,
+                author: String,
+                price: Double) extends Product
+
+object Test extends App {
+  val books = List(Book("Java", "John", 12.99))
+  var i = Invoice(1, "Denise", books)
+  println(i)
+  i = i + Book("Scala", "Adam", 10.99)
+  println(i)
+}
