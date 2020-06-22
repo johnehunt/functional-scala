a21284b83690333e90ae10dc0f792f7207da44e8
John Hunt <john.hunt10@gmail.com>
2020-06-22 15:18:47 +0100

    Added 2 parameter constructor

--- 09-exceptions/src/com/jjh/custom/MyException.scala
+++ 09-exceptions/src/com/jjh/custom/MyException.scala
@@ -3,7 +3,7 @@ package com.jjh.custom
 /**
  * Custom Exception type.
  */
-class MyException extends RuntimeException { }
+class MyException(_msg: String = "", _exp: Throwable = null) extends RuntimeException(_msg, _exp)
 
 object ExceptionGenerationApp extends App {
     throw new MyException()
